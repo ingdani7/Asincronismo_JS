a784a7b82c0be52f3f1d4851aeb2d61da0c97c51
Daniel Perez <ingdani7@gmail.com>
2023-04-07 09:23:20 -0500

    first commit

---
+++ src/callback/index.js
@@ -0,0 +1,19 @@
+function sum(num1,num2){
+    return num1+num2;
+}
+
+function calc(num1,num2,callback){
+    return callback(num1,num2);
+};
+
+console.log(calc(16,21,sum));
+
+setTimeout(function(){
+    console.log('Hola Javascript');
+},2000);
+
+function grettin(name){
+    console.log(`Hola ${name}`);
+}
+
+setTimeout(grettin,2000,`Daniel`)
